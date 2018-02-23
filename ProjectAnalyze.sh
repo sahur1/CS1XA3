a4fb2279bbb02e0465f9a3225de1fdacce348d02
Ritwik Jain Sahu <sahur1@mills.cas.mcmaster.ca>
2018-02-23 01:41:32 -0500

    Added menu and second and third feature

--- ProjectAnalyze.sh
+++ ProjectAnalyze.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 
-
 compare_status(){
 	echo $(git status | sed -n 2p )
 }
 
+changes(){
+	git diff > changes.log
+	read -p "Changes saved. To view, enter y" Input2
+	if [ "$Input2" = "y" ]
+	then
+		cat changes.log
+	fi
+}
 
+todo(){
+		grep -r --exclude=todo.log --exclude=ProjectAnalyze.sh "#TODO" . > "todo.log"
+		read -p "To view todo.log, enter y" Input3
+		if [ "$Input3" = "y" ]
+		then 
+			cat todo.log
+		fi
+}
 
-if [ "$1" = "compare_status" ]
+echo "To check if local repo is up to date with remote repo, enter 1"
+echo "To put all uncomiited changes in changes.log, enter 2"
+echo "To put each line from every file of your project with the tag #TODO into a file todo.log, enter 3"
+echo "To check all haskell files for syntax errors and put the results into error.log, enter 4"
+
+read Input1
+
+if [ "$Input1" = "1" ]
 then
 	compare_status
-else
-	echo "Not available yet"
+elif [ "$Input1" = "2" ]
+then	
+	changes
+elif [ "$Input1" = "3" ]
+then
+	todo
 fi
-
-
-
-
-
