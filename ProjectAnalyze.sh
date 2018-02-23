02095e1f3174d1e18fd136c9d4085f61039c7fd7
Ritwik Jain Sahu <sahur1@mills.cas.mcmaster.ca>
2018-02-23 18:43:57 -0500

    Completed main functionality

--- ProjectAnalyze.sh
+++ ProjectAnalyze.sh
@@ -14,28 +14,48 @@ changes(){
 }
 
 todo(){
-		grep -r --exclude=todo.log --exclude=ProjectAnalyze.sh "#TODO" . > "todo.log"
-		read -p "To view todo.log, enter y" Input3
-		if [ "$Input3" = "y" ]
-		then 
-			cat todo.log
-		fi
+	grep -r --exclude=todo.log --exclude=ProjectAnalyze.sh "#TODO" . > "todo.log"
+	read -p "To view todo.log, enter y" Input3
+	if [ "$Input3" = "y" ]
+	then 
+		cat todo.log
+	fi
 }
 
-echo "To check if local repo is up to date with remote repo, enter 1"
-echo "To put all uncomiited changes in changes.log, enter 2"
-echo "To put each line from every file of your project with the tag #TODO into a file todo.log, enter 3"
-echo "To check all haskell files for syntax errors and put the results into error.log, enter 4"
+haskell_errors(){
+	find . -name "*.hs" -type f -exec ghc -fno-code {} \; 2> error.log
+	read -p "To view error.log, enter y" Input4
+	if [ "$Input4" = "y" ]
+	then
+		cat error.log
+	fi
+}
 
-read Input1
+while
 
-if [ "$Input1" = "1" ]
-then
-	compare_status
-elif [ "$Input1" = "2" ]
-then	
-	changes
-elif [ "$Input1" = "3" ]
-then
-	todo
-fi
+	echo "To check if local repo is up to date with remote repo, enter 1"
+	echo "To put all uncomiited changes in changes.log, enter 2"
+	echo "To put each line from every file of your project with the tag #TODO into a fil	e todo.log, enter 3"
+	echo "To check all haskell files for syntax errors and put the results into error.lo	g, enter 4"
+
+	read Input1
+
+	if [ "$Input1" = "1" ]
+	then
+		compare_status
+	elif [ "$Input1" = "2" ]
+	then	
+		changes
+	elif [ "$Input1" = "3" ]
+	then
+		todo
+	elif [ "$Input1" = "4" ]
+	then
+		haskell_errors
+	fi
+	read -p "Would you like to continue (Y/N): " Input
+	[ "$Input" = "y" ]
+do
+	:
+done
+
